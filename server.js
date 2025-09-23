090302b740cab3ed8dabab6867dc74b24ca4e36f
Ken <kenpascualjacob@gmail.com>
2025-09-23 19:21:09 +0800

    Update server.js to modify mail options

--- server.js
+++ server.js
@@ -51,9 +51,17 @@ app.post("/send-report", upload.single("image"), async (req, res) => {
   let mailOptions = {
     from: process.env.GMAIL_USER,
     to: [
-      "macazo.386667@novaliches.sti.edu.ph",
-      "melendez.387472@novaliches.sti.edu.ph",
-      "acuna.385093@novaliches.sti.edu.ph"
+      "macazo.386667@novaliches.sti.edu.ph", 
+      "acuna.385093@novaliches.sti.edu.ph", 
+      "melendez.387472@novaliches.sti.edu.ph", 
+      "sandiego.393481@novaliches.sti.edu.ph", 
+      "kamimura.405642@novaliches.sti.edu.ph", 
+      "ramos.386137@novaliches.sti.edu.ph", 
+      "ramilo.385674@novaliches.sti.edu.ph", 
+      "go.387381@novaliches.sti.edu.ph", 
+      "nanquilada.388026@novaliches.sti.edu.ph", 
+      "paner.391196@novaliches.sti.edu.ph", 
+      "villegas.386021@novaliches.sti.edu.ph"
     ],
     subject: "New Lost and Found Report",
     text: `
